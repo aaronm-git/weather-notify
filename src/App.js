6064f06ac13397db16942e6356a664a1ed8ad362
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2021-09-02 20:55:24 -0400

    header component started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import Header from "./components/layout/Header";
+import { Container, Row, Col } from "react-bootstrap";
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row>
+          <Col md={4} className="vh-100 w-100 bg-dark"></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
